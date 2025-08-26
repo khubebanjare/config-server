8b5431184bfd4f2d2769d5c4a62586c3628079f4
khubebanjare <info.khube@gmail.com>
2025-08-26 21:37:58 +0530

    config-server has been created.

---
+++ src/test/java/org/khube/main/ConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khube.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
